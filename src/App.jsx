f15dabbc83fd091f71829ff7a2eaf094f54f2f0a
Monika <mciskowska@gmail.com>
2025-01-23 22:43:06 +0000

    displayed flashcards in horisonal view

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
       <div>
         <Header />
       </div>
-      <div>
+      <div className="parent">
         <FlashCard
           name={"What is React?"}
           picture={"🧐"}
