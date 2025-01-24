c8ee01c0c40debe22c5663c3d0e291ef785d3e15
Monika <mciskowska@gmail.com>
2025-01-24 08:11:31 +0000

    added some of the styles

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,10 @@
 //import { useState } from 'react'
 import HeaderIcon from "../components/header.png";
+import styles  from "./header.module.css";
 
 export function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <img src={HeaderIcon} alt="Header Icon" />
     </header>
   );
